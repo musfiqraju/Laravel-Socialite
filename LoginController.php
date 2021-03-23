63456dbac9f5fcd73464a175c6ebd84533ede96b
Muhammad Musfiqur Rahman <39381635+musfiqraju@users.noreply.github.com>
2021-03-23 14:54:43 +0600

    Add files via upload

---
+++ LoginController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Laravel\Socialite\Facades\Socialite;
+use Hash;
+use Str;
+use Auth;
+use App\Models\User;
+use userFromTokenAndSecret;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function github(){
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function githubRedirect(){
+        $user = Socialite::driver('github')->user();
+
+// OAuth Two Providers
+    $user = Socialite::driver('github')->user();
+    
+    $user = User::firstOrCreate(['email' => $user->email],
+            ['name' => $user->name,
+            'password' => Hash::make(Str::random(24))
+            
+    ]);
+
+        Auth::login($user,true);
+
+        return redirect('home');
+
+    }
+
+    
+}
